741fea3d53c951a357fb698ad0cc8ac221958f69
RobertoCraparo <craparoroberto@gmail.com>
2026-03-20 11:52:18 +0100

    connessione wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
+#include <WiFi.h>
+const char* ssid = "Mi 10T Lite";
+const char* password = "";
 
 Servo motoreServo;
 
 // Sostituisci questo valore con il pin effettivo.
 // Puoi usare un intero (es. 18 per D18) o la costante (es. D4 se la board lo mappa).
 const int pinSegnale = 18;
+
+
+
+void setup_wifi() {
+    // Configura il chip Wi-Fi in modalità client
+    WiFi.mode(WIFI_STA);
+
+    // Inizializza il processo di associazione al router
+    WiFi.begin(ssid, password);
+
+    // Loop bloccante che interroga lo stato della connessione ogni 500ms
+    // Il firmware non procede finché l'handshake WPA2 non è completato
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+    }
+}
 
 void setup() {
     // Alloca il timer hardware 0 per la generazione del segnale PWM
@@ -17,6 +36,8 @@ void setup() {
     // Associa il servomotore al pin specificato
     // Imposta la durata degli impulsi: 500us (0°) e 2400us (180°)
     motoreServo.attach(pinSegnale, 500, 2400);
+
+    setup_wifi();
 }
 
 void loop() {
