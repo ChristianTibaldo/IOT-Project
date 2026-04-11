b40ce038372b99fdf7aa1f432979ca35aafa9b6b
RobertoCraparo <craparoroberto@gmail.com>
2026-04-11 11:51:13 +0200

    aggiunto file per idee varie

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiClientSecure.h>
+#include <DHT.h>
 
-// Parametri hardware Servo
+// Configurazione sensori
+#define DHTPIN 33
+#define DHTTYPE DHT22
+#define PIRPIN 27 // Pin segnale modulo HC-SR505
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// Configurazione Servo
 const int servoPin = 18;
 const int pwmChannel = 0;
 const int pwmFreq = 50;
@@ -10,10 +18,10 @@ const int pwmResolution = 16;
 uint32_t dutyMin = 1638;
 uint32_t dutyMax = 8192;
 
-// Rete e MQTT
+// Credenziali e Server
 const char* ssid = "Mi 10T Lite";
 const char* wifi_password = "bccd15185a22x";
-const char* mqtt_server = "7b5efaa28c754223b5c5ae9bce2d6384.s1.eu.hivemq.cloud";
+const char* mqtt_server = "0b9a48980b684944aabfe4adc2ebc36b.s1.eu.hivemq.cloud";
 constexpr int mqtt_port = 8883;
 const char* mqtt_user = "IOT-Project";
 const char* mqtt_password = "Ccmt1234";
@@ -21,12 +29,11 @@ const char* mqtt_password = "Ccmt1234";
 WiFiClientSecure wifiClient;
 PubSubClient client(wifiClient);
 
-// Variabili di stato movimento
 float angoloAttuale = 0;
 int angoloTarget = 0;
 unsigned long ultimoStep = 0;
+unsigned long ultimoTentativoMqtt = 0;
 const int velocita = 15;
-
 int ora = 0;
 
 int wrap(int amt, int min, int max) {
@@ -43,15 +50,13 @@ void impostaAngolo(int nuovoAngolo) {
     angoloTarget = constrain(nuovoAngolo, 0, 270);
 }
 
-void gestisciMovimento() {
+void gestisciMovimentoServo() {
     if (millis() - ultimoStep >= velocita) {
         ultimoStep = millis();
-
         if (angoloAttuale < angoloTarget) {
             angoloAttuale += 1;
             impostaPWM(angoloAttuale);
-        }
-        else if (angoloAttuale > angoloTarget) {
+        } else if (angoloAttuale > angoloTarget) {
             angoloAttuale -= 1;
             impostaPWM(angoloAttuale);
         }
@@ -70,51 +75,69 @@ void impostaWifi() {
         delay(500);
         Serial.print(".");
     }
-    Serial.println("\nWiFi connesso");
+    Serial.println("\nWiFi OK");
 }
 
-void riconnetti() {
-    while (!client.connected()) {
-        if (client.connect("esp32-123", mqtt_user, mqtt_password)) {
-            client.subscribe("esp32/comandi");
-        } else {
-            delay(5000);
+void gestisciConnessioneMQTT() {
+    if (!client.connected()) {
+        if (millis() - ultimoTentativoMqtt > 5000) {
+            ultimoTentativoMqtt = millis();
+            if (client.connect("esp32-123", mqtt_user, mqtt_password)) {
+                client.subscribe("esp32/comandi");
+            }
         }
+    } else {
+        client.loop();
     }
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
     String msg = "";
     for (unsigned int i = 0; i < length; i++) msg += (char)payload[i];
-
-    if (String(topic) == "esp32/comandi") {
-        if (msg == "prossimaOra") {
-            prossimaOra();
-        }
+    if (String(topic) == "esp32/comandi" && msg == "prossimaOra") {
+        prossimaOra();
     }
 }
 
 void setup() {
     Serial.begin(115200);
+
+    // Configurazione I/O
+    pinMode(PIRPIN, INPUT);
+    dht.begin();
+
     ledcSetup(pwmChannel, pwmFreq, pwmResolution);
     ledcAttachPin(servoPin, pwmChannel);
-
     impostaPWM(angoloAttuale);
+
     impostaWifi();
     client.setServer(mqtt_server, mqtt_port);
     client.setCallback(callback);
 }
 
 void loop() {
-    if (!client.connected()) riconnetti();
-    client.loop();
+    gestisciConnessioneMQTT();
+    gestisciMovimentoServo();
 
-    gestisciMovimento();
-
-    // Invia dati a HiveMQ
     static unsigned long lastMsg = 0;
     if (millis() - lastMsg > 5000) {
         lastMsg = millis();
-        client.publish("esp32/dati", String(angoloAttuale).c_str());
+
+        float t = dht.readTemperature();
+        float h = dht.readHumidity();
+        // Lettura digitale dello stato del sensore PIR
+        bool movimento = digitalRead(PIRPIN);
+
+        String payload = "{";
+        payload += "\"temperatura\":" + (isnan(t) ? "null" : String(t));
+        payload += ",\"umidita\":" + (isnan(h) ? "null" : String(h));
+        payload += ",\"angolo\":" + String(angoloAttuale);
+        payload += ",\"movimento\":" + String(movimento ? "true" : "false");
+        payload += "}";
+
+        if (client.connected()) {
+            client.publish("esp32/dati", payload.c_str());
+            Serial.println("MQTT: " + payload);
+        }
     }
 }
